10217b7908bbd7270ec0f1b2ed2193e5ea4e9c56
sghivi <sghivi1@lsu.edu>
2024-09-15 23:56:09 -0500

    Finished

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,37 @@
 class PhoneBook
     def initialize
+        @list = Hash.new
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        if !@list[name].nil? then return false end
+        if is_listed
+            @list.each_value { |entry| 
+                if number.eql?(entry[0]) && entry[1] 
+                    return false 
+                end 
+            }
+        end
+        @list[name] = [number, is_listed]
+        return true
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        if (entry = @list[name]).nil? then return nil end
+        if entry[1] then return entry[0] end
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        @list.each_key { |key|
+            if (entry = @list[key])[1] && number.eql?(entry[0]) then return key end
+        }
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        arr = Array.new
+        @list.each_key { |key|
+            if areacode.eql?(@list[key][0].split("-")[0]) then arr.push(key) end
+        }
+        return arr
     end
 end
--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,49 @@
 def fib(n)
-    raise Exception, "Not Implemented"
+    result = Array.new()
+    if n > 0
+        result.push(0)
+        if n > 1
+          result.push(1)
+          i = 2
+          while i < n 
+            result.push(result[i-1]+result[i-2])
+            i += 1
+          end
+        end
+    end
+    return result
 end
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+    p = n.to_s()
+    q = p.reverse()
+    if (p <=> q) == 0 then return true else return false end
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+    if a.length() <= n then return nil end
+    return a.sort().reverse()[n]
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+    ar = s.chars
+    if ar.empty? then return "" end
+    hash = ar.to_h { |char| [char, 0] }
+    ar.each { |char| hash[char] += 1 }
+    maxk = nil
+    maxv = 0
+    hash.each_key { |key| if hash[key] > maxv then maxv = hash[key]; maxk = key end }
+    return maxk
 end
 
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+    if arr1.length != arr2.length then return nil end
+    i = -1
+    return arr1.to_h { |key| [key, arr2[i += 1]] }
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+    arr = Array.new
+    hash.each_key { |key| arr.push([key, hash[key]])}
+    return arr
 end
